3453b86b73aed6b9191347cad1daf3fb4d54685a
Nick Field <nickdfield@gmail.com>
2021-08-12 21:03:21 -0400

    simplify

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,25 +6,22 @@ set -u  # script fails if trying to access an undefined variable
 # Get arguments from action
 BASE_BRANCH="$1"
 DESTINATION_BRANCH="$2"
-REPLACEMENT_DIRECTORY="$3"
-COMMIT_USERNAME="$4"
-COMMIT_EMAIL="$5"
-COMMIT_MESSAGE="$6"
-
-# Use temp directory to clone into
-CLONE_DIRECTORY=$(mktemp -d)
+SOURCE_DIRECTORY="$3"
+TARGET_DIRECTORY="$4"
+COMMIT_USERNAME="$5"
+COMMIT_EMAIL="$6"
+COMMIT_MESSAGE="$7"
 
 # Setup git
 git config --global user.email "$COMMIT_EMAIL"
 git config --global user.name "$DESTINATION_USERNAME"
-git clone --single-branch --branch "$BASE_BRANCH" "https://$API_TOKEN_GITHUB@github.com/$DESTINATION_USERNAME/$DESTINATION_REPOSITORY.git" "$CLONE_DIRECTORY"
 
-cd "$CLONE_DIRECTORY"
-rm -rf "$REPLACEMENT_DIRECTORY"
+# replace the target directory
+rm -rf "$TARGET_DIRECTORY"
+cp -a "$SOURCE_DIRECTORY/." "$TARGET_DIRECTORY"
 
-cp -a "../$REPLACEMENT_DIRECTORY/." "$REPLACEMENT_DIRECTORY"
-
-git add .
+# Only commit the targeted directory
+git add "$TARGET_DIRECTORY/."
 git status
 
 # don't commit if no changes were made
@@ -32,3 +29,6 @@ git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
 
 # --set-upstream: sets the branch when pushing to a branch that does not exist
 git push origin --set-upstream "$DESTINATION_BRANCH"
+
+# Issue the github PR
+gh pr create --base $BASE_BRANCH --title "[ROBOT] Update" --body "Add the reason here"
