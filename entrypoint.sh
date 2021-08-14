9fc4139087d90d8cb658f54c2402b15f75542952
Nick Field <nicholas.field@iongroup.com>
2021-08-14 11:05:16 -0400

    minor changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 set -e  # if a command fails exit the script
 set -u  # script fails if trying to access an undefined variable
 
+echo "Setting up arguments"
 # Get arguments from action
 BASE_BRANCH="$1"
 DESTINATION_BRANCH="$2"
@@ -12,18 +13,20 @@ COMMIT_USERNAME="$5"
 COMMIT_EMAIL="$6"
 COMMIT_MESSAGE="$7"
 
-# Setup git
+echo "Setting up git"
 git config --global user.email "$COMMIT_EMAIL"
 git config --global user.name "$DESTINATION_USERNAME"
 
+echo "Building clone target directory"
 CLONE_DIRECTORY=$(mktemp -d)
 git clone --single-branch --branch "$BASE_BRANCH" "https://$API_TOKEN_GITHUB@github.com/$GITHUB_REPOSITORY.git" "$CLONE_DIRECTORY"
 
-# replace the target directory
+echo "Replacing target directory"
 rm -rf "$CLONE_DIRECTORY/$TARGET_DIRECTORY"
 cp -rvf $SOURCE_DIRECTORY "$CLONE_DIRECTORY/$TARGET_DIRECTORY"
 
-# Commit changes
+
+echo "Commiting changes"
 cd "$CLONE_DIRECTORY"
 git add .
 
@@ -33,5 +36,5 @@ git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
 # --set-upstream: sets the branch when pushing to a branch that does not exist
 git push origin --set-upstream "$DESTINATION_BRANCH"
 
-# Issue the github PR
+echo "Issuing PR"
 gh pr create --base $BASE_BRANCH --title "[ROBOT] Update" --body "Add the reason here"
